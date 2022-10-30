93cd8244a1763029840b19c0f599bcc59cb70fde
Holger Lersch <holger@lersch.net>
2022-10-30 09:29:49 -0400

    restricted to GET only, added some new fields to the entity

---
+++ migrations/Version20221030132844.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221030132844 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE news ADD abstract VARCHAR(255) NOT NULL, ADD body VARCHAR(255) NOT NULL, ADD id_news INT NOT NULL, CHANGE id id INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE news DROP abstract, DROP body, DROP id_news, CHANGE id id INT AUTO_INCREMENT NOT NULL');
+    }
+}
--- src/Entity/News.php
+++ src/Entity/News.php
@@ -2,6 +2,9 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+
 use Doctrine\ORM\Mapping as ORM;
 
 
@@ -11,19 +14,31 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity()
  * */
 
-#[ApiResource] 
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection()
+    ]
+)]
 class News
 {
 
-    /**
-     * The id of a message
-     * 
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
+   /**
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="SEQUENCE")
+    * @ORM\SequenceGenerator(sequenceName="id", initialValue=54574)
+    * @ORM\Column(type="integer")
+    */
     private ?int $id = null;
+    /**
+     *  @return string
+     */
+    public function getId(): int 
+    {
+        return $this->id;
+    }
     
+
     
     /**
      * @ORM\Column
@@ -40,6 +55,70 @@ class News
      *  @param string $headline
      */
     public function setHeadline(?string $headline): void  {
-        $this->headline = $name;
+        $this->headline = $headline;
+    }
+
+
+    /**
+     * @ORM\Column
+     */
+    private string $abstract = '';
+    /**
+     *  @return string
+     */
+    public function getAbstract(): string 
+    {
+        return $this->abstract;
+    }
+    /**
+     *  @param string $abstract
+     */
+    public function setAbstract(?string $abstract): void  {
+        $this->abstract = $abstract;
+    }
+
+
+    /**
+     * @ORM\Column
+     * @ORM\Column(type="text")
+     */
+    private string $body = '';
+    /**
+     *  @return string
+     */
+    public function getBody(): string 
+    {
+        return $this->body;
+    }
+    /**
+     *  @param string $body
+     */
+    public function setBody(?string $body): void  {
+        $this->body = $body;
+    }    
+
+    /**
+     * @ORM\Column
+     */
+    private int $idNews;
+
+    /**
+     * Get the value of idNews, the identifiert from the old database
+     */ 
+    public function getIdNews()
+    {
+        return $this->idNews;
+    }
+
+    /**
+     * Set the value of idNews
+     *
+     * @return  self
+     */ 
+    public function setIdNews($idNews)
+    {   
+        $this->idNews = $idNews;
+
+        return $this;
     }
 }
